cd0fcba04494067bb8ba44438d8687cd96b669bc
gazbert <gazbert@users.noreply.github.com>
2014-03-09 13:54:08 +0000

    Added Actor section in jdoc

--- src/java/com/gazbert/patterns/behavioural/state/package-info.java
+++ src/java/com/gazbert/patterns/behavioural/state/package-info.java
@@ -16,6 +16,16 @@
  * Very similar to the Strategy pattern: the main difference is the State pattern changes its behaviour based on
  * its state, whereas the Strategy pattern changes it behaviour using a different algorithm.
  * 
+ * <h2>Actors</h2>
+ * <ul>
+ * <li>State interface - defines the operation for all Concrete State implementations to provide impl for its current
+ *                       state</li>
+ * <li>Concrete State - provides the implementation for its state (getCost)</li>
+ * <li>Context - Creates all the Concrete States. Always holds a reference to the current State.
+ *               Provides the business methods.</li>
+ * <li>Client - Uses the Context to call the business methods. This is the test class. </li>
+ * </ul>
+ * 
  * <h2>Usages</h2>
  * 
  * Workflow mostly, e.g. a trading system, bug tracker, ...
