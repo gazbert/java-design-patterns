7c25b33d33f64fd7e4bfa05a98b6a5a89b51c28c
gazbert <gazbert@users.noreply.github.com>
2014-04-27 22:01:01 +0100

    Fixed indentation and added MIT license for State pattern - missed the test class.

--- src/test/com/gazbert/patterns/behavioural/state/TestStatePattern.java
+++ src/test/com/gazbert/patterns/behavioural/state/TestStatePattern.java
@@ -17,48 +17,48 @@ public class TestStatePattern
      */
     @Test
     public void testStatePattern() {
-	
-	int supportActualCostToDate = 0;
-	int supportExpectedCostToDate = 0;
-	
-	// Create the state machine
-	final BugTrackerContext bugTracker = new BugTrackerContext();	
-	
-	// remember, initial state is Customer Support @ £80 per hour...
-	supportExpectedCostToDate = 8*80;
-	bugTracker.logHoursWorked(8);
-	supportActualCostToDate = bugTracker.getCostOfFixingBugForCurrentState();
-	assertEquals(supportExpectedCostToDate, supportActualCostToDate);
 
-	// support do some more work
-	supportExpectedCostToDate += 4*80;
-	bugTracker.logHoursWorked(4);
-	supportActualCostToDate = bugTracker.getCostOfFixingBugForCurrentState();
-	assertEquals(supportExpectedCostToDate, supportActualCostToDate);
-	bugTracker.finishedWork(); //transition state
-	
-	// over to engineering - expensive! @ £100 per hour	
-	int engineeringActualCostToDate = 0;
-	int engineeringExpectedCostToDate = 0;
-	
-	engineeringExpectedCostToDate += 16*100;
-	bugTracker.logHoursWorked(16);
-	engineeringActualCostToDate = bugTracker.getCostOfFixingBugForCurrentState();
-	assertEquals(engineeringExpectedCostToDate, engineeringActualCostToDate);
-	bugTracker.finishedWork(); //transition state
-	
-	// over to testing @ £90 per hour
-	int testingActualCostToDate = 0;
-	int testingExpectedCostToDate = 0;
-	
-	testingExpectedCostToDate += 24*90;
-	bugTracker.logHoursWorked(24);
-	testingActualCostToDate = bugTracker.getCostOfFixingBugForCurrentState();
-	assertEquals(testingExpectedCostToDate, testingActualCostToDate);
-	bugTracker.finishedWork(); //transition state
-	
-	// Manager runs her reports...
-	assertEquals(supportExpectedCostToDate + engineeringExpectedCostToDate + testingExpectedCostToDate,
-		bugTracker.getTotalCostOfFixingBug());	
+        int supportActualCostToDate = 0;
+        int supportExpectedCostToDate = 0;
+
+        // Create the state machine
+        final BugTrackerContext bugTracker = new BugTrackerContext();	
+
+        // remember, initial state is Customer Support @ £80 per hour...
+        supportExpectedCostToDate = 8*80;
+        bugTracker.logHoursWorked(8);
+        supportActualCostToDate = bugTracker.getCostOfFixingBugForCurrentState();
+        assertEquals(supportExpectedCostToDate, supportActualCostToDate);
+
+        // support do some more work
+        supportExpectedCostToDate += 4*80;
+        bugTracker.logHoursWorked(4);
+        supportActualCostToDate = bugTracker.getCostOfFixingBugForCurrentState();
+        assertEquals(supportExpectedCostToDate, supportActualCostToDate);
+        bugTracker.finishedWork(); //transition state
+
+        // over to engineering - expensive! @ £100 per hour	
+        int engineeringActualCostToDate = 0;
+        int engineeringExpectedCostToDate = 0;
+
+        engineeringExpectedCostToDate += 16*100;
+        bugTracker.logHoursWorked(16);
+        engineeringActualCostToDate = bugTracker.getCostOfFixingBugForCurrentState();
+        assertEquals(engineeringExpectedCostToDate, engineeringActualCostToDate);
+        bugTracker.finishedWork(); //transition state
+
+        // over to testing @ £90 per hour
+        int testingActualCostToDate = 0;
+        int testingExpectedCostToDate = 0;
+
+        testingExpectedCostToDate += 24*90;
+        bugTracker.logHoursWorked(24);
+        testingActualCostToDate = bugTracker.getCostOfFixingBugForCurrentState();
+        assertEquals(testingExpectedCostToDate, testingActualCostToDate);
+        bugTracker.finishedWork(); //transition state
+
+        // Manager runs her reports...
+        assertEquals(supportExpectedCostToDate + engineeringExpectedCostToDate + testingExpectedCostToDate,
+                bugTracker.getTotalCostOfFixingBug());	
     }
 }
