c84374186137be57d507357e1405cac2395d3504
gazbert <gazbert@users.noreply.github.com>
2014-04-27 22:35:06 +0100

    Fixed indentation and added MIT license for Static Factory pattern.

--- src/test/com/gazbert/patterns/creational/staticfactorymethod/TestStaticFactoryMethodPattern.java
+++ src/test/com/gazbert/patterns/creational/staticfactorymethod/TestStaticFactoryMethodPattern.java
@@ -1,12 +1,32 @@
 package com.gazbert.patterns.creational.staticfactorymethod;
 
+/*The MIT License (MIT)
+
+Copyright (c) 2014 Gazbert
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
+
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 import com.gazbert.patterns.creational.factorymethod.products.Product;
 import com.gazbert.patterns.creational.staticfactorymethod.InvestmentFundFactory;
-
 
 /**
  * Demonstrates use of Static Factory Method / Simple Factory pattern.
@@ -25,20 +45,20 @@ public class TestStaticFactoryMethodPattern
     @Test (expected = IllegalArgumentException.class)
     public void testCreatingInvestmentFundProducts() throws Exception 
     {
-	final Product oeic = InvestmentFundFactory.build(true, InvestmentFundFactory.ProductType.OEIC);
-	assertFalse(oeic.isTaxable());
-	assertTrue(oeic.getProductDetails().equals("OEIC"));
-	
-	final Product gilt = InvestmentFundFactory.build(true, InvestmentFundFactory.ProductType.GILT);
-	assertFalse(gilt.isTaxable());
-	assertTrue(gilt.getProductDetails().equals("Gilt"));
-	
-	final Product trust = InvestmentFundFactory.build(false, InvestmentFundFactory.ProductType.INVESTMENT_TRUST);
-	assertTrue(trust.isTaxable()); // trusts cannot be in ISA
-	assertTrue(trust.getProductDetails().equals("Investment Trust"));
-	
-	// Now try building something illegal
-	InvestmentFundFactory.build(false, InvestmentFundFactory.ProductType.CASH_SAVINGS_ACCOUNT);
+        final Product oeic = InvestmentFundFactory.build(true, InvestmentFundFactory.ProductType.OEIC);
+        assertFalse(oeic.isTaxable());
+        assertTrue(oeic.getProductDetails().equals("OEIC"));
+
+        final Product gilt = InvestmentFundFactory.build(true, InvestmentFundFactory.ProductType.GILT);
+        assertFalse(gilt.isTaxable());
+        assertTrue(gilt.getProductDetails().equals("Gilt"));
+
+        final Product trust = InvestmentFundFactory.build(false, InvestmentFundFactory.ProductType.INVESTMENT_TRUST);
+        assertTrue(trust.isTaxable()); // trusts cannot be in ISA
+        assertTrue(trust.getProductDetails().equals("Investment Trust"));
+
+        // Now try building something illegal
+        InvestmentFundFactory.build(false, InvestmentFundFactory.ProductType.CASH_SAVINGS_ACCOUNT);
     }
 }
    
