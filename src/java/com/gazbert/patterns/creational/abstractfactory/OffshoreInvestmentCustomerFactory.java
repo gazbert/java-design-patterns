5c99d095db4144fcb94fc5a31dc8aee7df926cc6
gazbert <gazbert@users.noreply.github.com>
2014-04-27 22:14:52 +0100

    Fixed indentation and added MIT license for Abstract Factory pattern..

--- src/java/com/gazbert/patterns/creational/abstractfactory/OffshoreInvestmentCustomerFactory.java
+++ src/java/com/gazbert/patterns/creational/abstractfactory/OffshoreInvestmentCustomerFactory.java
@@ -1,5 +1,26 @@
 package com.gazbert.patterns.creational.abstractfactory;
 
+/*The MIT License (MIT)
+
+Copyright (c) 2014 Gazbert
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
+
 import com.gazbert.patterns.creational.abstractfactory.account.Account;
 import com.gazbert.patterns.creational.abstractfactory.account.PlatinumAccount;
 import com.gazbert.patterns.creational.abstractfactory.address.Address;
@@ -17,22 +38,22 @@ public class OffshoreInvestmentCustomerFactory extends AbstractCustomerFactory {
 
     @Override
     public Address createAddress() {
-	
-	// my Bermuda address
-	return new OffshoreAddress();
+
+        // my Bermuda address
+        return new OffshoreAddress();
     }
 
     @Override
     public Account createAccount() {
-	
-	// offshore customers have special accounts
-	return new PlatinumAccount();
+
+        // offshore customers have special accounts
+        return new PlatinumAccount();
     }
 
     @Override
     public Product createProduct() {
-	
-	// zarro capital gains... :-)
-	return new TaxExemptInvestmentFund();
+
+        // zarro capital gains... :-)
+        return new TaxExemptInvestmentFund();
     }
 }
--- src/test/com/gazbert/patterns/creational/abstractfactory/TestAbstractFactoryPattern.java
+++ src/test/com/gazbert/patterns/creational/abstractfactory/TestAbstractFactoryPattern.java
@@ -1,5 +1,26 @@
 package com.gazbert.patterns.creational.abstractfactory;
 
+/*The MIT License (MIT)
+
+Copyright (c) 2014 Gazbert
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
+
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -19,78 +40,78 @@ public class TestAbstractFactoryPattern
     /** For sake of demo and id'ing what customer to create - the App business logic would decide this */
     private static enum CustomerType
     {
-	OFFSHORE_INVESTOR,
-	
-	ONSHORE_INVESTOR;
+        OFFSHORE_INVESTOR,
+
+        ONSHORE_INVESTOR;
     }
-    
+
     /**
      * Some previous bit of code logic in the App decides we want to create an Offshore investment customer.
      */
     @Test
     public void testCreatingOffshoreInvesmentCustomer() throws Exception {
-		
-	// App logic decides this in real life...
-	final CustomerType customerType = CustomerType.OFFSHORE_INVESTOR;
-	
-	final AbstractCustomerFactory abstractCustomeFactory;
-	
-	if (customerType == CustomerType.ONSHORE_INVESTOR)
-	{
-	    abstractCustomeFactory = new OnshoreInvestmentCustomerFactory();	    
-	}
-	else if (customerType == CustomerType.OFFSHORE_INVESTOR)
-	{
-	    abstractCustomeFactory = new OffshoreInvestmentCustomerFactory();
+
+        // App logic decides this in real life...
+        final CustomerType customerType = CustomerType.OFFSHORE_INVESTOR;
+
+        final AbstractCustomerFactory abstractCustomeFactory;
+
+        if (customerType == CustomerType.ONSHORE_INVESTOR)
+        {
+            abstractCustomeFactory = new OnshoreInvestmentCustomerFactory();	    
         }
-	else
-	{
-	    throw new IllegalArgumentException("Unknown customer type: " + customerType);
-	}
-	
-	// Create the Customer's parts - either and offshore investor or an onshore investor
-	final Account account = abstractCustomeFactory.createAccount();
-	final Address address = abstractCustomeFactory.createAddress();
-	final Product product = abstractCustomeFactory.createProduct();
-	
-	// What we got?
-	assertTrue(account.getAccountDetails().startsWith("Platinum"));
-	assertTrue(address.getAddressDetails().startsWith("Offshore"));
-	assertTrue(product.getProductDetails().startsWith("Investment Trust"));	
+        else if (customerType == CustomerType.OFFSHORE_INVESTOR)
+        {
+            abstractCustomeFactory = new OffshoreInvestmentCustomerFactory();
+        }
+        else
+        {
+            throw new IllegalArgumentException("Unknown customer type: " + customerType);
+        }
+
+        // Create the Customer's parts - either and offshore investor or an onshore investor
+        final Account account = abstractCustomeFactory.createAccount();
+        final Address address = abstractCustomeFactory.createAddress();
+        final Product product = abstractCustomeFactory.createProduct();
+
+        // What we got?
+        assertTrue(account.getAccountDetails().startsWith("Platinum"));
+        assertTrue(address.getAddressDetails().startsWith("Offshore"));
+        assertTrue(product.getProductDetails().startsWith("Investment Trust"));	
     }
-    
+
     /**
      * Some previous bit of code logic in the App decides we want to create an Onshore investment customer.
      */
     @Test
     public void testCreatingOnshoreInvesmentCustomer() throws Exception {
-		
-	// App logic decides this in real life...
-	final CustomerType customerType = CustomerType.ONSHORE_INVESTOR;
-	
-	final AbstractCustomerFactory abstractCustomeFactory;
-	
-	if (customerType == CustomerType.ONSHORE_INVESTOR)
-	{
-	    abstractCustomeFactory = new OnshoreInvestmentCustomerFactory();	    
-	}
-	else if (customerType == CustomerType.OFFSHORE_INVESTOR)
-	{
-	    abstractCustomeFactory = new OffshoreInvestmentCustomerFactory();
+
+        // App logic decides this in real life...
+        final CustomerType customerType = CustomerType.ONSHORE_INVESTOR;
+
+        final AbstractCustomerFactory abstractCustomeFactory;
+
+        if (customerType == CustomerType.ONSHORE_INVESTOR)
+        {
+            abstractCustomeFactory = new OnshoreInvestmentCustomerFactory();	    
         }
-	else
-	{
-	    throw new IllegalArgumentException("Unknown customer type: " + customerType);
-	}
-	
-	// Create the Customer's parts - either and offshore investor or an onshore investor
-	final Account account = abstractCustomeFactory.createAccount();
-	final Address address = abstractCustomeFactory.createAddress();
-	final Product product = abstractCustomeFactory.createProduct();
-	
-	// What we got?
-	assertTrue(account.getAccountDetails().startsWith("Standard"));
-	assertTrue(address.getAddressDetails().startsWith("Domicile"));
-	assertTrue(product.getProductDetails().startsWith("Taxable OEIC"));	
+        else if (customerType == CustomerType.OFFSHORE_INVESTOR)
+        {
+            abstractCustomeFactory = new OffshoreInvestmentCustomerFactory();
+        }
+        else
+        {
+            throw new IllegalArgumentException("Unknown customer type: " + customerType);
+        }
+
+        // Create the Customer's parts - either and offshore investor or an onshore investor
+        final Account account = abstractCustomeFactory.createAccount();
+        final Address address = abstractCustomeFactory.createAddress();
+        final Product product = abstractCustomeFactory.createProduct();
+
+        // What we got?
+        assertTrue(account.getAccountDetails().startsWith("Standard"));
+        assertTrue(address.getAddressDetails().startsWith("Domicile"));
+        assertTrue(product.getProductDetails().startsWith("Taxable OEIC"));	
     }
 }
