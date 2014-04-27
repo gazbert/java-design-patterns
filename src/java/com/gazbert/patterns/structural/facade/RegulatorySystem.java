4a5f91a80aa16725a11ac911c085c8283588da45
gazbert <gazbert@users.noreply.github.com>
2014-04-27 22:43:51 +0100

    Fixed indentation and added MIT license for Facade pattern.

--- src/java/com/gazbert/patterns/structural/facade/RegulatorySystem.java
+++ src/java/com/gazbert/patterns/structural/facade/RegulatorySystem.java
@@ -1,5 +1,26 @@
 package com.gazbert.patterns.structural.facade;
 
+/*The MIT License (MIT)
+
+Copyright (c) 2014 Gazbert
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
+
 /**
  * Regulatory system makes sure the trade activity complies with the Treasury and SEC regulations.
  * 
@@ -10,6 +31,6 @@ public class RegulatorySystem {
 
     public void auditTrade(final TradeDetails trade)
     {
-	System.out.println("Checking trade for regulatory compliance...");
+        System.out.println("Checking trade for regulatory compliance...");
     }
 }
