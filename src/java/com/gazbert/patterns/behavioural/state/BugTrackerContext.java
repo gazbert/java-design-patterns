e782731796744283db3b47db88f2560548eb4e86
gazbert <gazbert@users.noreply.github.com>
2014-04-27 21:58:15 +0100

    Fixed indentation and added MIT license for State pattern.

--- src/java/com/gazbert/patterns/behavioural/state/BugTrackerContext.java
+++ src/java/com/gazbert/patterns/behavioural/state/BugTrackerContext.java
@@ -1,5 +1,25 @@
 package com.gazbert.patterns.behavioural.state;
 
+/*The MIT License (MIT)
+
+Copyright (c) 2014 Gazbert
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
 
 /**
  * The context for a *very* simple bug tracker.  The example shows how the State pattern can be used for keeping
@@ -16,21 +36,21 @@ public class BugTrackerContext {
     
     /** Initial state */
     private BugState customerSupportState;
-    
+
     /** Another state... */
     private BugState engineeringState;
-    
+
     /** Another state... */
     private BugState testingState;
-    
+
     /** Terminal state. */
     private BugState closedState;
-    
-    
+
+
     /** The current state */
     private BugState currentState;
-    
-    
+
+
     /**
      * Constructor initialises the state context.
      * <p>
@@ -40,79 +60,79 @@ public class BugTrackerContext {
      */
     public BugTrackerContext()
     {
-	customerSupportState = new CustomerSupportState(this);
-	engineeringState = new EngineeringState(this);
-	testingState = new TestingState(this);
-	closedState = new ClosedState(this);	
-	
-	// Set initial state
-	currentState = customerSupportState;	
+        customerSupportState = new CustomerSupportState(this);
+        engineeringState = new EngineeringState(this);
+        testingState = new TestingState(this);
+        closedState = new ClosedState(this);	
+
+        // Set initial state
+        currentState = customerSupportState;	
     }
-    
+
     public void setState(final BugState state)    
     {
-	currentState = state;
+        currentState = state;
     }
-        
+
     /////////////// State Accessors //////////////////
-    
+
     public BugState CustomerSupportState()
     {
-	return customerSupportState;
+        return customerSupportState;
     }
-    
+
     public BugState getEngineeringState()
     {
-	return engineeringState;
+        return engineeringState;
     }
-    
+
     public BugState getTestingState()
     {
-	return testingState;
+        return testingState;
     }
-    
+
     public BugState getClosedState()
     {
-	return closedState;
+        return closedState;
     }
 
     /////////////// Business Methods //////////////////    
-    
+
     /**
      * Users log their hours on the bug.
      * @param hours
      */
     public void logHoursWorked(int hours)
     {
-	currentState.updateHoursWorked(hours);
+        currentState.updateHoursWorked(hours);
     }
-    
+
     /**
      * Users notify system they have finished working on bug.
      */
     public void finishedWork()
     {
-	currentState.setNextState();
+        currentState.setNextState();
     }
-    
+
     /**
      * Managers want to know cost of bug fix in current state.
      * @return
      */
     public int getCostOfFixingBugForCurrentState()
     {
-	return currentState.getCost();
+        return currentState.getCost();
     }     
-    
+
     /**
      * Managers want to know total cost of bug fix
      * @return
      */
     public int getTotalCostOfFixingBug()
     {
-	return customerSupportState.getCost() 
-		+ engineeringState.getCost()
-		+ testingState.getCost()
-		+  closedState.getCost();
+        return customerSupportState.getCost() 
+                + engineeringState.getCost()
+                + testingState.getCost()
+                +  closedState.getCost();
     }        
 }
--- src/java/com/gazbert/patterns/behavioural/state/EngineeringState.java
+++ src/java/com/gazbert/patterns/behavioural/state/EngineeringState.java
@@ -1,5 +1,25 @@
 package com.gazbert.patterns.behavioural.state;
 
+/*The MIT License (MIT)
+
+Copyright (c) 2014 Gazbert
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
 
 /**
  * Bug is in Engineering...
@@ -11,13 +31,13 @@ public class EngineeringState implements BugState {
 
     /** Cost per hour for Engineering work */
     private static final int HOURLY_RATE = 100;
-    
+
     /** Reference to the context */
     private BugTrackerContext bugTracker;
 
     /** Internal state of this object - Cost of the bug */
     private int bugCost;
-    
+
     /**
      * Constructor takes the context as an arg.
      * 
@@ -25,24 +45,24 @@ public class EngineeringState implements BugState {
      */
     public EngineeringState(final BugTrackerContext bugTracker)
     {
-	this.bugTracker = bugTracker;
-	
-	// init this object's internal state
-	bugCost = 0;
+        this.bugTracker = bugTracker;
+
+        // init this object's internal state
+        bugCost = 0;
     }
-    
+
     public void updateHoursWorked(int hoursWorked) {
-	
-	bugCost += (hoursWorked * HOURLY_RATE);
+
+        bugCost += (hoursWorked * HOURLY_RATE);
     }
-    
+
     public int getCost() {	
-	
-	return bugCost;	
+
+        return bugCost;	
     }
 
     @Override
     public void setNextState() {
-	bugTracker.setState(bugTracker.getTestingState()); // control which state we go to next	
+        bugTracker.setState(bugTracker.getTestingState()); // control which state we go to next	
     }
 }
