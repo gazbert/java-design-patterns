3678464053c9f3d5eb7375b307822d664673de10
gazbert <gazbert@users.noreply.github.com>
2014-04-27 22:37:29 +0100

    Fixed indentation and added MIT license for Adaper  pattern.

--- src/java/com/gazbert/patterns/structural/adapter/AbstractAccount.java
+++ src/java/com/gazbert/patterns/structural/adapter/AbstractAccount.java
@@ -1,5 +1,26 @@
 package com.gazbert.patterns.structural.adapter;
 
+/*The MIT License (MIT)
+
+Copyright (c) 2014 Gazbert
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
+
 import java.math.BigDecimal;
 
 
@@ -13,34 +34,34 @@ public class AbstractAccount implements Account {
 
     private BigDecimal balance;
     private boolean isOverdraftAvailable;
-    
+
     public AbstractAccount(final BigDecimal size, boolean isOverdraftAvailable)
     {
-	this.balance = size;
-	this.isOverdraftAvailable = isOverdraftAvailable;	
+        this.balance = size;
+        this.isOverdraftAvailable = isOverdraftAvailable;	
     }
-    
+
     @Override
     public BigDecimal getBalance() {
-	
-	return balance;
+
+        return balance;
     }
 
     @Override
     public boolean isOverdraftAvailable() {
 
-	return isOverdraftAvailable;
+        return isOverdraftAvailable;
     }
 
     @Override
     public String toString()
     {
-	return getClass().getSimpleName() + " Balance=" + getBalance() + " Overdraft:" + isOverdraftAvailable();
+        return getClass().getSimpleName() + " Balance=" + getBalance() + " Overdraft:" + isOverdraftAvailable();
     }
 
     @Override
     public void creditAccount(final BigDecimal credit) {
-	
-	balance = balance.add(credit);	
+
+        balance = balance.add(credit);	
     }
 }
--- src/java/com/gazbert/patterns/structural/adapter/thirdparty/OffshoreAccount.java
+++ src/java/com/gazbert/patterns/structural/adapter/thirdparty/OffshoreAccount.java
@@ -1,7 +1,27 @@
 package com.gazbert.patterns.structural.adapter.thirdparty;
 
-import java.math.BigDecimal;
+/*The MIT License (MIT)
 
+Copyright (c) 2014 Gazbert
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
+
+import java.math.BigDecimal;
 
 /**
  * The 3rd party API class - in real-life, we would not have access to the source, probably just the 
@@ -16,40 +36,40 @@ public class OffshoreAccount {
 
     private BigDecimal balance;
     private boolean canHaveNegativeBalance;
-    
+
     /** The tax for the country where the account is */
     private static final BigDecimal TAX_RATE = new BigDecimal(0.03);
-    
+
     public OffshoreAccount(final BigDecimal size, boolean canHaveNegativeBalance)
     {
-	this.balance = size;
-	this.canHaveNegativeBalance = canHaveNegativeBalance;	
+        this.balance = size;
+        this.canHaveNegativeBalance = canHaveNegativeBalance;	
     }
-    
+
     public BigDecimal getGrossBalance() {
-	
-	return balance;
+
+        return balance;
     }
-    
+
     public BigDecimal getOffshoreTaxRate() {
-	
-	return TAX_RATE;
+
+        return TAX_RATE;
     }
-   
+
     public boolean canHaveNegativeBalance() {
 
-	return canHaveNegativeBalance;
+        return canHaveNegativeBalance;
     }
 
     public void creditTheAccount(final BigDecimal credit)
     {
-	balance = balance.add(credit);
+        balance = balance.add(credit);
     }
-    
+
     @Override
     public String toString()
     {
-	return getClass().getSimpleName() + " Balance=" + getGrossBalance() + " -veBalanceAllowed:"
+        return getClass().getSimpleName() + " Balance=" + getGrossBalance() + " -veBalanceAllowed:"
                 + canHaveNegativeBalance();
     }
 }
