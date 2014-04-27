bf420c25cc6e4c1ddf10828a85c92331d2db2354
gazbert <gazbert@users.noreply.github.com>
2014-04-27 22:33:56 +0100

    Fixed indentation and added MIT license for Singleton pattern.

--- src/test/com/gazbert/patterns/creational/singleton/TestSingletonPattern.java
+++ src/test/com/gazbert/patterns/creational/singleton/TestSingletonPattern.java
@@ -1,9 +1,29 @@
 package com.gazbert.patterns.creational.singleton;
 
+/*The MIT License (MIT)
+
+Copyright (c) 2014 Gazbert
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
+
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-
 
 /**
  * Demonstrates use of Singleton pattern.
@@ -19,36 +39,36 @@ public class TestSingletonPattern
     @Test
     public void testUsingEnumSingleton() throws Exception 
     {
-	final SingletonUsingEnum firstAccessAttempt = SingletonUsingEnum.SINGLE_INSTANCE;
-	final SingletonUsingEnum secondAccessAttempt = SingletonUsingEnum.SINGLE_INSTANCE;
-	
-	// Same object? Hope so!
-	assertTrue(firstAccessAttempt == secondAccessAttempt); // reference same object
-	assertTrue(firstAccessAttempt.equals(secondAccessAttempt)); // and equality the same in this case
-	
-	// Use the Singleton to so something that mandates only 1 instance of the class e.g
-	// for controlling sequential access to writing a log file
-	firstAccessAttempt.log("Logging some data...");
+        final SingletonUsingEnum firstAccessAttempt = SingletonUsingEnum.SINGLE_INSTANCE;
+        final SingletonUsingEnum secondAccessAttempt = SingletonUsingEnum.SINGLE_INSTANCE;
+
+        // Same object? Hope so!
+        assertTrue(firstAccessAttempt == secondAccessAttempt); // reference same object
+        assertTrue(firstAccessAttempt.equals(secondAccessAttempt)); // and equality the same in this case
+
+        // Use the Singleton to so something that mandates only 1 instance of the class e.g
+        // for controlling sequential access to writing a log file
+        firstAccessAttempt.log("Logging some data...");
     }
-    
+
     /**
      * Shows the 'classic' way of using an <em>eager</em> Singleton.
      */
     @Test
     public void testUsingClassicEagerSingleton() throws Exception 
     {
-	final ClassicEagerSingleton firstAccessAttempt = ClassicEagerSingleton.getInstance();
-	final ClassicEagerSingleton secondAccessAttempt = ClassicEagerSingleton.getInstance();
-	
-	// Same object? Hope so!
-	assertTrue(firstAccessAttempt == secondAccessAttempt); // reference same object
-	assertTrue(firstAccessAttempt.equals(secondAccessAttempt)); // and equality the same in this case
-	
-	// Use the Singleton to so something that mandates only 1 instance of the class e.g
-	// for controlling sequential access to writing a log file
-	firstAccessAttempt.log("Logging some data...");
+        final ClassicEagerSingleton firstAccessAttempt = ClassicEagerSingleton.getInstance();
+        final ClassicEagerSingleton secondAccessAttempt = ClassicEagerSingleton.getInstance();
+
+        // Same object? Hope so!
+        assertTrue(firstAccessAttempt == secondAccessAttempt); // reference same object
+        assertTrue(firstAccessAttempt.equals(secondAccessAttempt)); // and equality the same in this case
+
+        // Use the Singleton to so something that mandates only 1 instance of the class e.g
+        // for controlling sequential access to writing a log file
+        firstAccessAttempt.log("Logging some data...");
     }
-    
+
     /**
      * Shows the 'classic' way of using an <em>lazy</em> Singleton using Double Checked Locking.
      * <p>
@@ -57,20 +77,20 @@ public class TestSingletonPattern
     @Test
     public void testUsingClassicLazySingletonUsingDoubleCheckedLocking() throws Exception 
     {
-	final ClassicLazySingletonUsingDoubleCheckedLocking firstAccessAttempt =
-		ClassicLazySingletonUsingDoubleCheckedLocking.getInstance();
-	final ClassicLazySingletonUsingDoubleCheckedLocking secondAccessAttempt = 
-		ClassicLazySingletonUsingDoubleCheckedLocking.getInstance();
-	
-	// Same object? Hope so!
-	assertTrue(firstAccessAttempt == secondAccessAttempt); // reference same object
-	assertTrue(firstAccessAttempt.equals(secondAccessAttempt)); // and equality the same in this case
-	
-	// Use the Singleton to so something that mandates only 1 instance of the class e.g
-	// for controlling sequential access to writing a log file
-	firstAccessAttempt.log("Logging some data...");
+        final ClassicLazySingletonUsingDoubleCheckedLocking firstAccessAttempt =
+                ClassicLazySingletonUsingDoubleCheckedLocking.getInstance();
+        final ClassicLazySingletonUsingDoubleCheckedLocking secondAccessAttempt = 
+                ClassicLazySingletonUsingDoubleCheckedLocking.getInstance();
+
+        // Same object? Hope so!
+        assertTrue(firstAccessAttempt == secondAccessAttempt); // reference same object
+        assertTrue(firstAccessAttempt.equals(secondAccessAttempt)); // and equality the same in this case
+
+        // Use the Singleton to so something that mandates only 1 instance of the class e.g
+        // for controlling sequential access to writing a log file
+        firstAccessAttempt.log("Logging some data...");
     }
-    
+
     /**
      * Shows the 'classic' way of using an <em>lazy</em> Singleton using synchronized method.
      * <p>
@@ -80,18 +100,18 @@ public class TestSingletonPattern
     @Test
     public void testUsingClassicLazySingletonUsingSynchronizedMethod() throws Exception 
     {
-	final ClassicLazySingletonUsingSynchronizedMethod firstAccessAttempt =
-		ClassicLazySingletonUsingSynchronizedMethod.getInstance();
-	final ClassicLazySingletonUsingSynchronizedMethod secondAccessAttempt = 
-		ClassicLazySingletonUsingSynchronizedMethod.getInstance();
-	
-	// Same object? Hope so!
-	assertTrue(firstAccessAttempt == secondAccessAttempt); // reference same object
-	assertTrue(firstAccessAttempt.equals(secondAccessAttempt)); // and equality the same in this case
-	
-	// Use the Singleton to so something that mandates only 1 instance of the class e.g
-	// for controlling sequential access to writing a log file
-	firstAccessAttempt.log("Logging some data...");
+        final ClassicLazySingletonUsingSynchronizedMethod firstAccessAttempt =
+                ClassicLazySingletonUsingSynchronizedMethod.getInstance();
+        final ClassicLazySingletonUsingSynchronizedMethod secondAccessAttempt = 
+                ClassicLazySingletonUsingSynchronizedMethod.getInstance();
+
+        // Same object? Hope so!
+        assertTrue(firstAccessAttempt == secondAccessAttempt); // reference same object
+        assertTrue(firstAccessAttempt.equals(secondAccessAttempt)); // and equality the same in this case
+
+        // Use the Singleton to so something that mandates only 1 instance of the class e.g
+        // for controlling sequential access to writing a log file
+        firstAccessAttempt.log("Logging some data...");
     }
 }
-   
+
